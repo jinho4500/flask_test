8b0c5568aff4e9a0b2cdc5ae6521da24f5880c19
jinho4500 <39111185+jinho4500@users.noreply.github.com>
2023-03-24 15:50:22 +0900

    feat: 에러찾기

--- basic/d4.py
+++ basic/d4.py
@@ -1,38 +1,46 @@
 '''
-    데이터 베이스 접속후 쿼리 수행
+    데이터 베이스 접속후 쿼리 수행 + 파라미터 전달
 '''
 import pymysql as my
 
-connection = None
-try:    
-    connection = my.connect(host        ='localhost',
-                            #port        = 3306,     
-                            user        ='root',     
-                            password    ='12341234', 
-                            database    ='ml_db',    
-                            # 조회 결과는 [ {}, {}, {},...  ] 이런 형태로 추출된다                            
-                            cursorclass =my.cursors.DictCursor
-                            )
-    with connection.cursor() as cursor: # cursor는 with문을 벗어나면 자동으로 닫힘
-        sql = '''
-            SELECT 
-                `name`, uid, regdate 
-            FROM 
-                users
-            WHERE
-                uid='guest'
-            AND
-                upw='1234';
-        '''
-        # execute() 함수의 2번 인자가 파라미터 전달하는 자리, 튜플로 표현
-        cursor.execute( sql,('guest','1234') )
-        row = cursor.fetchone()
-        print( row['name'] )
-        pass
-except Exception as e:
-    print('접속 오류', e)
-else:
-    print('접속시 문제 없었음')
-finally:    
+def select_login():
+    connection = None
+    try:    
+        connection = my.connect(host        ='localhost',
+                                #port        = 3306,     
+                                user        ='root',     
+                                password    ='12341234', 
+                                database    ='ml_db',                                
+                                cursorclass =my.cursors.DictCursor
+                                )
+        with connection.cursor() as cursor:
+            # 파라미터는 %s표시로 순서대로 세팅된다 '값' => ''는 자동으로 세팅된다
+            sql = '''
+                SELECT 
+                    `name`, uid, regdate 
+                FROM 
+                    users
+                WHERE
+                    uid=%s
+                AND
+                    upw=%s;
+            '''
+            # execute() 함수의 2번 인자가 파라미터 전달하는 자리, 튜플로 표현
+            cursor.execute( sql, ('guest', '1234') )
+            row = cursor.fetchone()        
+            print( row['name'] )
+            pass
+    except Exception as e:
+        print('접속 오류', e)
+    else:
+        print('접속시 문제 없었음')
+    finally:    
+        if connection:
+            connection.close()    
+
+if __name__ == '__main__':
+    # d4 개발자의 테스트 코드
+    # f5 개발자가 사용할때는 작동않함
+    select_login()
     if connection:
-        connection.close()    
+        connection.close()
