25ae5e9cd4c9d04fad1663d15a933f783d8c73e6
jinho4500 <39111185+jinho4500@users.noreply.github.com>
2023-04-05 13:28:25 +0900

    feat: DB

--- advance/step2_app_factory/service/controllers/auth_controller.py
+++ advance/step2_app_factory/service/controllers/auth_controller.py
@@ -3,8 +3,13 @@
     - 라우트 : URL과 이를 처리할 함수 연계
     - 비즈니스 로직 : 사용가 요청하는 주 내용을 처리한는 곳
 '''
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, jsonify
 from service.controllers import bp_auth as auth
+# 시간정보획득, 시간차를 계산하는 함수
+from datetime import datetime, timedelta
+# Flask 객체 획득
+from flask import current_app
+import jwt
 
 # ~/auth
 @auth.route('/')
@@ -13,9 +18,31 @@ def home():
     print( url_for('auth_bp.login') )
     return "auth 홈"
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return "auth login"
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        # jwt 관련 체크 => 정상(200), 오류(401)
+        # 1. uid, upw 획득
+        uid = request.form.get('uid')
+        upw = request.form.get('upw')
+        # 2. uid, upw로 회원이 존재하는지 체크->(원래디비, 임시로값비교)
+        if uid=='guest' and upw=='1234':
+            # 3. 회원이면 토큰 생성 (규격, 만료시간, 암호알고리즘 지정,..)
+            payload = { 
+                # 저장할 정보는 알아서 구성(고객 정보가 기반)
+                'id':uid,
+                # 만료시간 (원하는대로 설정)
+                # 토큰이 발급되고 나서 + 24시간 후에 토큰은 만료된다
+                'exp':datetime.utcnow() + timedelta(seconds=60*60*24)
+            }
+            # 토큰 발급 => 시크릿키, 해시알고리즘("HS256"), 데이터(payload)
+            SECRET_KEY = current_app.config['SECRET_KEY'] # 환경변수값 획득
+            # 발급
+            token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+            # 4. 응답 전문 구성 -> 응답
+            return jsonify( {'code':1, 'token':token} )
 
 @auth.route('/logout')
 def logout():
@@ -23,6 +50,12 @@ def logout():
 
 @auth.route('/signup')
 def signup():
+    # TODO : 비밀번호 암호화
+    password = '1234'
+    # 암호화
+    b = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    # 확인 및 복호화
+    print(password, b, bcrypt.checkpw(password.encode('utf-8'),b))
     return "auth signup"
 
 @auth.route('/delete')
--- advance/step2_app_factory/service/controllers/main_controller.py
+++ advance/step2_app_factory/service/controllers/main_controller.py
@@ -3,16 +3,42 @@
     - 라우트 : URL과 이를 처리할 함수 연계
     - 비즈니스 로직 : 사용가 요청하는 주 내용을 처리한는 곳
 '''
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, Response
 from service.controllers import bp_main as main
 from service.forms import FormQuestion
+from flask import current_app
+
+import jwt
+import time
+from datetime import datetime
 
 # ~/main
 @main.route('/')
 def home():
+    #1. 쿠키중에 토큰 획득 -> 실패 -> 401
+    token = request.cookies.get('token')
+    SECRECT_KEY = current_app.config['SECRET_KEY']
+    if not token or not SECRECT_KEY:
+        return Response(status=401)
+    try:
+        # 2. 디로딩 -> 실패하면 ->401
+        payload = jwt.decode(token, SECRECT_KEY, algorithms=['HS256'])
+        # 3. 유효 날짜 추출, 현재 시간 기준보다 과거인지 체크 => 과거라면:만료 -> 401
+        if payload['exp'] < time.mktime(datetime.utcnow().timetuple()):
+            return Response(status=401)
+
+    except jwt.InvalidTokenError:
+        return Response(status=401)
+    except jwt.ExpiredSignatureError:
+        return Response(status=401)
+    except jwt.exceptions.DecodeError:
+        return Response(status=401)
     return render_template('index.html')
 
-@main.route('/question')
+# ~/main/question
+@main.route('/question', methods=['GET','POST'])
 def question():
     form = FormQuestion()
+    if request.method == 'POST' and form.validate_on_submit():
+        return redirect( url_for('main_bp.home') ) # ~/main
     return render_template('question.html', wtf=form)
